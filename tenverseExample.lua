0045c82471c299498e8156c806fa67adadc84b87
Noah Bate <92116978+noahdoespython@users.noreply.github.com>
2022-03-01 20:33:22 +0000

    Create tenverseExample.lua

---
+++ tenverseExample.lua
@@ -0,0 +1,11 @@
+-- Importing TenverseAPI
+
+
+local tenverse = require(script.Main)
+--- Info
+
+tenverse.search("329991150712651776") --  <-- Fill that in with a VALID Discord ID.
+
+-- extras
+
+tenverse.raining_tacos(true) -- For a present
